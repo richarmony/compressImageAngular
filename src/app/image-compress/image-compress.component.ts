24bcdc658c319f91a3de865a55d496694ab4c73a
Ricardo Cuevas <richardtrex@gmail.com>
2020-08-02 13:16:43 -0500

    UPDATE arregle un poco el código

--- src/app/image-compress/image-compress.component.ts
+++ src/app/image-compress/image-compress.component.ts
@@ -9,67 +9,144 @@ import { NgxImageCompressService } from 'ngx-image-compress';
 export class ImageCompressComponent implements OnInit {
 
   constructor(private imageCompress: NgxImageCompressService) { }
-  file: any;
+  file: File;
   predictions: number[];
   imageDataEvent: any;
+
   localUrl: any;
-  localCompressedURl:any;
-  isFlip: boolean = false;
-  sizeOfOriginalImage:number;
-  sizeOFCompressedImage:number;
+  localCompressedURl: any;
+
+  isFlip = false;
+  sizeOfOriginalImage: number;
+  sizeOFCompressedImage: number;
+
+  imgResultBeforeCompress: string;
+  imgResultAfterCompress: string;
+
+
   selectFile(event: any) {
     // console.log(event.target.files);
-    var  fileName : any;
+    let  fileName: any;
     this.file = event.target.files[0];
-    fileName = this.file['name'];
-    console.log("file size:",this.file['size']/(1024*1024));
-    console.log("uploaded file:", this.file);
-    if (event.target.files && event.target.files[0]) {
-      var reader = new FileReader();
-      reader.onload = (event: any) => {
-        this.localUrl = event.target.result;
-        this.compressFile(this.localUrl,fileName)
-      }
-      reader.readAsDataURL(event.target.files[0]);
+    fileName = this.file.name;
+    console.log('file size:', this.file.size / (1024 * 1024));
+    console.log('uploaded file:', this.file);
+
+    if (event.target.files[0]) {
+      // if (event.target.files && event.target.files[0]) {
+      console.log('Will this execute?????');
+      let newFile = this.resizeImg(this.file);
+
+      console.log('DEBUG - newFile.size: ', newFile.size);
+      console.log('DEBUG - newFile.name: ', newFile.name);
+
+
+      // const reader = new FileReader();
+      // reader.onload = (resp: any) => {
+      // this.localUrl = resp.target.result;
+      // this.compressFile(this.localUrl, fileName);
+      // };
+      // reader.readAsDataURL(event.target.files[0]);
     }
-    
+
   }
-  imgResultBeforeCompress:string;
-  imgResultAfterCompress:string;
-  compressFile(image,fileName) {
-    var orientation = -1;
-    this.sizeOfOriginalImage = this.imageCompress.byteCount(image)/(1024*1024);
-      console.warn('Size in bytes is now:',  this.sizeOfOriginalImage);
-      
-      this.imageCompress.compressFile(image, orientation, 50, 50).then(
-        result => {
+
+  resizeImg(file: File) {
+    console.log('TODO: - resize image ;)');
+    // -- Read File:
+    const reader = new FileReader();
+    // let resizeFile: File;
+    reader.onload = (resp: any) => {
+      this.localUrl = resp.target.result;  // -- file raw data
+      // resizeFile = this.compressFile(this.localUrl, file);
+      this.compressFile(this.localUrl, file);
+
+      // console.log('DEBUG - resizeFile.name: ', resizeFile.name);
+      // console.log('DEBUG - resizeFile.size: ', resizeFile.size);
+    };
+    reader.readAsDataURL(file);
+
+
+
+    // let upperLimit = 1024 * 1024; // -- 1MB
+    let upperLimit = 1024 * 200; // -- 200 KB
+
+    if(file.size > upperLimit) {
+      console.log('Process image ;)');
+      // -- process file :O
+      return file;
+    } else {
+      console.log('Don\'t Process image ;)');
+      return file;
+    }
+  }
+
+  precompressFile(image, file: File) {
+    const fileName = file.name;
+    this.imageCompress.compressFile(image, orientation, 100, 50).then(
+      result => {
+        this.imgResultAfterCompress = result;
+        this.localCompressedURl = result;
+        this.sizeOFCompressedImage = this.imageCompress.byteCount(result) / (1024 * 1024);
+        // console.warn('Size in bytes after compression:',  this.sizeOFCompressedImage);
+        // create file from byte
+        const imageName = fileName;
+        // call method that creates a blob from dataUri
+        const imageBlob = this.dataURItoBlob(this.imgResultAfterCompress.split(',')[1]);
+        const imageFile = new File([result], imageName, { type: 'image/jpeg' });
+        // console.log('file size:', imageFile.size / (1024 * 1024));
+        console.warn('preprocess file size:', imageFile.size / 1024);
+        // console.warn('Size in bytes after compression:',  this.sizeOFCompressedImage);
+        // return imageFile;
+      }
+    );
+  }
+
+  compressFile(image, file: File) {
+    const orientation = -1;
+    const imageName = file.name;
+
+    this.sizeOfOriginalImage = file.size / 1024;
+    // this.sizeOfOriginalImage = this.imageCompress.byteCount(image) / 1.333416054;
+    // this.sizeOfOriginalImage = this.imageCompress.byteCount(image) / 1.333416054;
+    // this.sizeOfOriginalImage = this.imageCompress.byteCount(image) / (8);
+    console.warn('Size in bytes is now:',  this.sizeOfOriginalImage);
+
+    let currentSize = file.size;
+    let desiredSize = 200 * 1024; // -- 200 KB
+    // let desiredSize = 1 * 512 * 1024; // -- 500 KB
+
+    let ratio = (desiredSize / currentSize) * 100;
+    // let ratio = 100 * desiredSize / currentSize;
+    // let ratio = (currentSize / desiredSize) * 100;
+    // let ratio = 100 * currentSize / (desiredSize);
+
+    if (currentSize < desiredSize) {
+      ratio = 100;
+    }
+    console.log('currentSize:',  currentSize);
+    console.log('desiredSize:',  desiredSize);
+    console.log('Ratio:',  ratio);
+
+
+
+    let newFile: File;
+    this.imageCompress.compressFile(image, orientation, ratio, 50)
+        .then( result => {
           this.imgResultAfterCompress = result;
           this.localCompressedURl = result;
-          this.sizeOFCompressedImage = this.imageCompress.byteCount(result)/(1024*1024)
-          console.warn('Size in bytes after compression:',  this.sizeOFCompressedImage);
+          this.sizeOFCompressedImage = this.imageCompress.byteCount(result) / (1024 * 1024);
+          // console.warn('Size in bytes after compression:',  this.sizeOFCompressedImage);
           // create file from byte
-          const imageName = fileName;
-          // call method that creates a blob from dataUri
-          const imageBlob = this.dataURItoBlob(this.imgResultAfterCompress.split(',')[1]);
           const imageFile = new File([result], imageName, { type: 'image/jpeg' });
-          console.log("file size:",imageFile['size']/(1024*1024));
-        }
-      );
-    // this.imageCompress.uploadFile().then(({image, orientation}) => {
-    //       console.log('Image:',image);
-    //       console.log('orientation:',orientation);
-    //   this.imgResultBeforeCompress = image;
-    //   console.warn('Size in bytes was:', this.imageCompress.byteCount(image));
-      
-    //   this.imageCompress.compressFile(image, orientation, 50, 50).then(
-    //     result => {
-    //       this.imgResultAfterCompress = result;
-    //       console.warn('Size in bytes is now:', this.imageCompress.byteCount(result));
-    //     }
-    //   );
-      
-    // });
-    
+          // console.log('file size:', imageFile.size / (1024 * 1024));
+          console.warn('file size:', imageFile.size / 1024);
+          newFile =  imageFile;
+          console.log('DEBUG - newFile.name: ', newFile.name);
+          console.log('DEBUG - newFile.size: ', newFile.size);
+          return newFile;
+          // console.warn('Size in bytes after compression:',  this.sizeOFCompressedImage);
+        });
   }
    dataURItoBlob(dataURI) {
     const byteString = window.atob(dataURI);
@@ -78,7 +155,7 @@ export class ImageCompressComponent implements OnInit {
     for (let i = 0; i < byteString.length; i++) {
       int8Array[i] = byteString.charCodeAt(i);
     }
-    const blob = new Blob([int8Array], { type: 'image/jpeg' });    
+    const blob = new Blob([int8Array], { type: 'image/jpeg' });
     return blob;
  }
   ngOnInit() {
